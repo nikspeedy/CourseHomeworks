98af63bac52eb5724b8109501b3873da1eb9373c
nikspeedy <ns_dimitrov@abv..bg>
2015-07-08 22:04:51 +0300

    EXAM

---
+++ EXAM/EXAM/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter random 9 numbers from 2 to 9");
+            int n = int.Parse(Console.ReadLine());
+            int[,] array1 = new int[n,n];
+
+           for (int i = 0; i < array1.GetLength(0);i++)
+			{
+			 for (int j = 0; j < array1.GetLength(1); j++)
+			{
+                 array1 [i,j] = int.Parse(Console.ReadLine());
+			}
+			}
+            int counter = 0;
+            int[]array2 = new int[n*n];
+            for (int i = 0; i < array1.GetLength(0); i++)
+            {
+                for (int j = 0; j < array1.GetLength(1); j++)
+                {
+                    array2[counter++] = array1[j, i];
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.WriteLine(array2[i]);
+            }
+        }
+    }
+}
